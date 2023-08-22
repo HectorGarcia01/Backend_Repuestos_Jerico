1238594ea2b7e645fef7c82727cf7659c3541a97
HectorGarcia01 <hg2779948@gmail.com>
2023-08-22 04:48:48 -0600

    Creación del esquema de validación usuario, o sea utilizado tanto para cliente como para empleado

---
+++ src/schemas/user.schema.js
@@ -0,0 +1,68 @@
+const Joi = require('joi');
+const customError = require('../utils/custom_error');
+
+/**
+ * Esquema de validación de datos de Cliente y Empleado
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              custom_error.js (para errores personalizados)
+ */
+
+const userSchema = Joi.object({
+    nombre: Joi.string()
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("El nombre es obligatorio.", error);
+        }),
+    apellido: Joi.string()
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("El apellido es obligatorio.", error);
+        }),
+    telefono: Joi.string()
+        .pattern(new RegExp('^[345][0-9]{7}'))
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("El teléfono es obligatorio y debe de ser válido.", error);
+        }),
+    nit: Joi.number()
+        .integer()
+        .error((error) => {
+            return customError("El NIT debe ser numérico.", error);
+        }),
+    correo: Joi.string()
+        .email({ tlds: { allow: ['com'] } })
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("El correo electrónico es obligatorio, debe de tener la extensión 'com' y debe ser válido.", error);
+        }),
+    password: Joi.string()
+        .pattern(new RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*\\s).*$'))
+        .min(8)
+        .max(25)
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("La contraseña es obligatoria y debe tener al menos 8 carácteres, al menos una letra mayúscula, una letra minúscula, un número y no puede contener espacios.", error);
+        }),
+    repetir_password: Joi.string()
+        .valid(Joi.ref('Password_Cliente'))
+        .required()
+        .error((error) => {
+            return customError("Las contraseñas no coinciden.", error);
+        }),
+    departamento: Joi.string()
+        .trim(),
+    municipio: Joi.string()
+        .trim(),
+    direccion_referencia: Joi.string()
+        .trim()
+});
+
+//Exportación del esquema de validación
+module.exports = userSchema;
