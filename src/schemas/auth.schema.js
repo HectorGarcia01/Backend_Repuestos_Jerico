72d985b9efa2939375bc0ecd73afd80ad9eb374d
HectorGarcia01 <hg2779948@gmail.com>
2023-08-22 04:37:41 -0600

    Creación del esquema de validación para las credenciales del login

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+const customError = require('../utils/custom_error');
+
+/**
+ * Esquema de validación de credenciales
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              custom_error.js (para errores personalizados)
+ */
+
+const loginSchema = Joi.object({
+    correo: Joi.string()
+        .email({ tlds: { allow: ['com'] } })
+        .required()
+        .trim()
+        .error((error) => {
+            return customError("Correo inválido.", error);
+        }),
+    password: Joi.string()
+        .required()
+        .trim()
+});
+
+//Exportación del esquema de validación para el login
+module.exports = loginSchema;
