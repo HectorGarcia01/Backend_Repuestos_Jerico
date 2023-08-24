7955a5cb26b785dd6a6b3bcb23a53783306efffa
HectorGarcia01 <hg2779948@gmail.com>
2023-08-24 14:11:02 -0600

    Configuración de la ruta (endpoint) para cerrar la sesión actual del usuario

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    login
+    login,
+    logout
 } = require('../controllers/auth.controller');
 const authSchema = require('../schemas/auth.schema');
 const validateMiddleware = require('../middlewares/validate');
+const authMiddleware = require('../middlewares/auth');
 
 router.post('/usuario/login', validateMiddleware(authSchema), login);
+router.post('/usuario/logout', authMiddleware, logout);
 
 //Exportación de todas las rutas de autenticación
 module.exports = router;
