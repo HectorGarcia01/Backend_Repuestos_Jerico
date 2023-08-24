52ee65d02559bb28a766a2a17f01a48db440f015
HectorGarcia01 <hg2779948@gmail.com>
2023-08-23 19:26:22 -0600

    Creación del modelo empleado con sus respectivos métodos personalizados como la generación de tokens de autenticación, filtrado de información no relevante y la búsqueda por sus credenciales, adempás de la creación de un Hook para el cifrado de contraseña antes de realizar el registro a la bd

---
+++ src/models/employee.js
@@ -0,0 +1,132 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/db_connection');
+const { KEY_TOKEN } = require('../config/config');
+
+/**
+ * Creación del modelo Empleado
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Estado (state.js).
+ *              Modelo Rol (role.js).
+ */
+
+const Empleado = db.define('Empleado', {
+    nombre: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING(8),
+        allowNull: false
+    },
+    nit: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    correo: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        low: true,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    foto_perfil: {
+        type: DataTypes.BLOB,
+        allowNull: true
+    },
+    ID_Estado_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Estados',
+            key: 'id'
+        }
+    },
+    ID_Rol_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Rols',
+            key: 'id'
+        }
+    }
+});
+
+/**
+ * Hook para el cifrado de contraseña
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: Datos del empleado actual
+ */
+
+Empleado.beforeCreate(async (employee) => {
+    employee.password = await bcrypt.hash(employee.password, 8);
+});
+
+/**
+ * Método personalizado para generar tokens
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: Variable de entorno para llave secreta (config.js)
+ */
+
+Empleado.prototype.generateAuthToken = (id, role) => {
+    const token = jwt.sign({ id: id.toString(), role }, KEY_TOKEN);
+    return token;
+};
+
+/**
+ * Método personalizado para validar credenciales
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ */
+
+Empleado.prototype.findByCredentials = async (correo, password) => {
+    const employee = await Empleado.findOne({
+        where: {
+            correo
+        }
+    });
+
+    if (!employee) {
+        return false;
+    }
+
+    const isMatch = await bcrypt.compare(password, employee.password);
+
+    if (!isMatch) {
+        throw new Error("Credenciales inválidas.");
+    }
+
+    return employee;
+};
+
+/**
+ * Método personalizado para filtrar información
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ */
+
+Empleado.prototype.toJSON = function () {
+    const employee = { ...this.get() };
+
+    delete employee.foto_perfil;
+    delete employee.password;
+    delete employee.createdAt;
+    delete employee.updatedAt;
+    delete employee.ID_Rol_FK;
+
+    return employee;
+};
+
+module.exports = Empleado;
