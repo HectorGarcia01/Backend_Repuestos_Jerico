73f4ebfda980ee9e5e594ebe1ef4225e09e4f913
HectorGarcia01 <hg2779948@gmail.com>
2023-08-24 05:30:09 -0600

    Creación de la función para crear un nuevo empleado

---
+++ src/controllers/employee.controller.js
@@ -0,0 +1,92 @@
+const Sequelize = require('sequelize');
+const EmployeeModel = require('../models/employee');
+const AddressModel = require('../models/address');
+const RoleModel = require('../models/role');
+const StateModel = require('../models/state');
+const TokenModel = require('../models/token');
+
+/**
+ * Función para crear un nuevo empleado
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Empleado (employee.js), 
+ *              Modelo Direccion (address.js), 
+ *              Modelo Rol (role.js), 
+ *              Modelo Estado (state.js)
+ *              Modelo Token (token.js)
+ */
+
+const createEmployee = async (req, res) => {
+    try {
+        const {
+            nombre,
+            apellido,
+            telefono,
+            nit,
+            correo,
+            password,
+            departamento,
+            municipio,
+            direccion_referencia
+        } = req.body;
+
+        const stateEmployee = await StateModel.findOne({
+            where: {
+                nombre_estado: 'Pendiente'
+            }
+        });
+
+        if (!stateEmployee) {
+            return res.status(404).send({ error: "Estado no encontrado." });
+        }
+
+        const roleEmployee = await RoleModel.findOne({
+            where: {
+                nombre_rol: 'Admin'
+            }
+        });
+
+        if (!roleEmployee) {
+            return res.status(404).send({ error: "Rol no encontrado." });
+        }
+
+        const addEmployee = await EmployeeModel.create({
+            nombre,
+            apellido,
+            telefono,
+            nit,
+            correo,
+            password,
+            ID_Estado_FK: stateEmployee.id,
+            ID_Rol_FK: roleEmployee.id
+        });
+
+        if (departamento || municipio || direccion_referencia) {
+            await AddressModel.create({
+                departamento,
+                municipio,
+                direccion_referencia,
+                ID_Empleado_FK: addEmployee.id
+            });
+        }
+
+        const token = await addEmployee.generateAuthToken(addEmployee.id, roleEmployee.nombre_rol);
+        await TokenModel.create({
+            token_usuario: token,
+            ID_Empleado_FK: addEmployee.id
+        });
+
+        res.status(201).send({ msg: "Se ha registrado con éxito." });
+    } catch (error) {
+        if (error instanceof Sequelize.UniqueConstraintError) {
+            res.status(400).send({ error: "¡El empleado ya existe!" });
+        } else {
+            res.status(500).send({ error: "Error interno del servidor." });
+        }
+    }
+};
+
+module.exports = {
+    createEmployee
+};
