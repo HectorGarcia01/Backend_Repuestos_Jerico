343cb949a52fc8d352a8da3771bd618e091852d2
HectorGarcia01 <hg2779948@gmail.com>
2023-09-06 00:26:31 -0600

    Configuración de las rutas (endpoints) del superAdmin para ver todos los empleados, ver empleado por su id y borrar de forma lógica un empleado por su id

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -3,7 +3,10 @@ const router = new express.Router();
 const {
     createEmployee,
     readProfile,
-    updateEmployee
+    updateEmployee,
+    readEmployees,
+    readEmployeeId,
+    deleteEmployeeId
 } = require('../controllers/employee.controller');
 const employeeSchema = require('../schemas/user.schema');
 const validateMiddleware = require('../middlewares/validate');
@@ -19,14 +22,13 @@ router.post(
     createEmployee
 );
 router.get('/superAdmin/ver/perfil', authMiddleware, roleMiddleware('SuperAdmin'), readProfile);
-// router.get('/superAdmin/ver/empleados', authMiddleware, roleMiddleware('SuperAdmin'), readEmployees);
-// router.get('/superAdmin/ver/empleado/:id', authMiddleware, roleMiddleware('SuperAdmin'), readEmployeeId);
+router.get('/superAdmin/ver/empleados', authMiddleware, roleMiddleware('SuperAdmin'), readEmployees);
+router.get('/superAdmin/ver/empleado/:id', authMiddleware, roleMiddleware('SuperAdmin'), readEmployeeId);
 router.patch('/superAdmin/actualizar/perfil', authMiddleware, roleMiddleware('SuperAdmin'), updateEmployee);
-// router.delete('/superAdmin/eliminar/empleado/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteEmployeeId);
+router.delete('/superAdmin/eliminar/empleado/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteEmployeeId);
 
 //Configuración de rutas (endpoints) para el Admin
 router.get('/admin/ver/perfil', authMiddleware, roleMiddleware('Admin'), readProfile);
 router.patch('/admin/actualizar/perfil', authMiddleware, roleMiddleware('Admin'), updateEmployee);
 
-//Exportación de todas las rutas de empleado
 module.exports = router;
