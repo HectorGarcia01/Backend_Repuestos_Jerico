29bf26527412ed650db9c2c4ed2f06447b1fee54
HectorGarcia01 <hg2779948@gmail.com>
2023-09-06 00:52:59 -0600

    Eliminación de llaves foráneas en el modelo Direccion

--- src/models/address.js
+++ src/models/address.js
@@ -5,39 +5,16 @@ const db = require('../database/db_connection');
  * Creación del modelo Direccion
  * Fecha creación: 22/08/2023
  * Autor: Hector Armando García González
- * Referencias:
- *              Modelo Cliente (customer.js)
- *              Modelo Empleado (employee.js)
  */
 
 const Direccion = db.define('Direccion', {
     departamento: {
-        type: DataTypes.STRING(20),
+        type: DataTypes.STRING(30),
         allowNull: true
     },
     municipio: {
-        type: DataTypes.STRING(20),
+        type: DataTypes.STRING(30),
         allowNull: true
-    },
-    direccion_referencia: {
-        type: DataTypes.STRING(40),
-        allowNull: true
-    },
-    ID_Cliente_FK: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-            model: 'Clientes',
-            key: 'id'
-        }
-    },
-    ID_Empleado_FK: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-            model: 'Empleados',
-            key: 'id'
-        }
     }
 });
 
@@ -50,8 +27,6 @@ const Direccion = db.define('Direccion', {
 Direccion.prototype.toJSON = function () {
     const address = { ...this.get() };
 
-    delete address.ID_Cliente_FK;
-    delete address.ID_Empleado_FK;
     delete address.createdAt;
     delete address.updatedAt;
 
