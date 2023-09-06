80edfb913c9037e2aa080c9efe6722d09b9d1b51
HectorGarcia01 <hg2779948@gmail.com>
2023-09-06 00:14:55 -0600

    Configuración de las rutas (endpoints) para ver categorías por su id, actualizar datos de categoría por su id con su respectivo esquema de validación de datos y middleware de validación, además de la ruta (endpoint) para eliminar de forma lógica una categoría que solo el superAdmin tiene acceso

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,22 +1,60 @@
 const express = require('express');
 const router =  new express.Router();
 const {
-    addCategory,
-    readCategories
+    createCategory,
+    readCategories,
+    readCategoryId,
+    updateCategoryId,
+    deleteCategoryId
 } = require('../controllers/category.controller');
 const {
-    categorySchema
+    categorySchema,
+    updateCategorySchema
 } = require('../schemas/inventory.schema');
-const validate = require('../middlewares/validate');
+const validateMiddleware = require('../middlewares/validate');
 const authMiddleware = require('../middlewares/auth');
 const roleMiddleware = require('../middlewares/check_role');
 
 //Rutas (endpoints para categoría)
-router.post('/superAdmin/crear/categoria', authMiddleware, roleMiddleware('SuperAdmin'), validate(categorySchema), addCategory);
-router.post('/admin/crear/categoria', authMiddleware, roleMiddleware('Admin'), validate(categorySchema), addCategory);
+router.post(
+    '/superAdmin/crear/categoria', 
+    authMiddleware, 
+    roleMiddleware('SuperAdmin'), 
+    validateMiddleware(categorySchema), 
+    createCategory
+);
+router.post(
+    '/admin/crear/categoria', 
+    authMiddleware, 
+    roleMiddleware('Admin'), 
+    validateMiddleware(categorySchema), 
+    createCategory
+);
+
 router.get('/superAdmin/ver/categorias', authMiddleware, roleMiddleware('SuperAdmin'), readCategories);
 router.get('/admin/ver/categorias', authMiddleware, roleMiddleware('Admin'), readCategories);
 router.get('/usuario/ver/categorias', readCategories);
+
+router.get('/superAdmin/ver/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), readCategoryId);
+router.get('/admin/ver/categoria/:id', authMiddleware, roleMiddleware('Admin'), readCategoryId);
+router.get('/usuario/ver/categoria/:id', readCategoryId);
+
+router.patch(
+    '/superAdmin/actualizar/categoria/:id', 
+    authMiddleware, 
+    roleMiddleware('SuperAdmin'), 
+    validateMiddleware(updateCategorySchema), 
+    updateCategoryId
+);
+router.patch(
+    '/admin/actualizar/categoria/:id', 
+    authMiddleware, 
+    roleMiddleware('Admin'), 
+    validateMiddleware(updateCategorySchema), 
+    updateCategoryId
+);
+
+router.delete('/superAdmin/eliminar/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteCategoryId);
 
 //Rutas (endpoints para productos)
 
