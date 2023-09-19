b1d73e44bd121b703c8834b32e121df891782fa6
HectorGarcia01 <hg2779948@gmail.com>
2023-09-19 02:32:24 -0600

    Creación del modelo Departamento

---
+++ src/models/department.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db_connection');
+
+/**
+ * Creación del modelo Departamento
+ * Fecha creación: 19/09/2023
+ * Autor: Hector Armando García González
+ */
+
+const Departamento = db.define('Departamento', {
+    nombre_departamento: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true
+    }
+});
+
+/**
+ * Método personalizado para filtrar información
+ * Fecha creación: 19/09/2023
+ * Autor: Hector Armando García González
+ */
+
+Departamento.prototype.toJSON = function () {
+    const department = { ...this.get() };
+
+    delete department.createdAt;
+    delete department.updatedAt;
+
+    return department;
+};
+
+module.exports = Departamento;
