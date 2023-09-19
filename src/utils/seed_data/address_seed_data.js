ec4f710a4a6d7bf21183ad34d6ceb5f1b9c57b6d
HectorGarcia01 <hg2779948@gmail.com>
2023-09-19 02:26:41 -0600

    Estableciendo datos de siembra para los departamentos y sus municipios

---
+++ src/utils/seed_data/address_seed_data.js
@@ -0,0 +1,445 @@
+//Exportación de direcciones
+module.exports = {
+    departamentos: [
+        {
+            nombre: 'Alta Verapaz',
+            municipios: [
+                'Cobán',
+                'San Pedro Carchá',
+                'San Juan Chamelco',
+                'Tactic',
+                'Tucurú',
+                'Panzós',
+                'Senahú',
+                'Santa María Cahabón',
+                'Lanquín',
+                'Chahal',
+                'Fray Bartolomé de las Casas',
+                'Chisec',
+                'Santa Cruz Verapaz',
+                'Cahabón',
+                'Tamahú'
+            ]
+        },
+        {
+            nombre: 'Baja Verapaz',
+            municipios: [
+                'Salamá',
+                'San Miguel Chicaj',
+                'Rabinal',
+                'Cubulco',
+                'Granados',
+                'Santa Cruz el Chol',
+                'San Jerónimo',
+                'Purulhá'
+            ]
+        },
+        {
+            nombre: 'Chimaltenango',
+            municipios: [
+                'Chimaltenango',
+                'San José Poaquil',
+                'San Martín Jilotepeque',
+                'Comalapa',
+                'Santa Apolonia',
+                'Tecpán Guatemala',
+                'Patzún',
+                'Pochuta',
+                'Patzicía',
+                'Santa Cruz Balanyá',
+                'Acatenango',
+                'Yepocapa',
+                'San Andrés Itzapa',
+                'Parramos',
+                'Zaragoza',
+                'El Tejar'
+            ]
+        },
+        {
+            nombre: 'Chiquimula',
+            municipios: [
+                'Chiquimula',
+                'San José La Arada',
+                'San Juan Ermita',
+                'Jocotán',
+                'Camotán',
+                'Olopa',
+                'Esquipulas',
+                'Quezaltepeque',
+                'San Jacinto',
+                'Ipala'
+            ]
+        },
+        {
+            nombre: 'El Progreso',
+            municipios: [
+                'Guastatoya',
+                'Morazán',
+                'San Agustín Acasaguastlán',
+                'San Cristóbal Acasaguastlán',
+                'El Jícaro',
+                'Sansare',
+                'Sanarate'
+            ]
+        },
+        {
+            nombre: 'Escuintla',
+            municipios: [
+                'Escuintla',
+                'Santa Lucía Cotzumalguapa',
+                'La Democracia',
+                'Siquinalá',
+                'Masagua',
+                'Tiquisate',
+                'La Gomera',
+                'Guanagazapa',
+                'San José',
+                'Iztapa',
+                'Palín',
+                'San Vicente Pacaya',
+                'Nueva Concepción'
+            ]
+        },
+        {
+            nombre: 'Guatemala',
+            municipios: [
+                'Ciudad de Guatemala',
+                'Villa Nueva',
+                'Mixco',
+                'San Juan Sacatepéquez',
+                'San José Pinula',
+                'San Pedro Ayampuc',
+                'Amatitlán',
+                'Villa Canales',
+                'Santa Catarina Pinula',
+                'San Miguel Petapa',
+                'Chinautla',
+                'Palencia',
+                'San José del Golfo',
+                'San José Acatempa',
+            ]
+        },
+        {
+            nombre: 'Huehuetenango',
+            municipios: [
+                'Huehuetenango',
+                'Chiantla',
+                'Malacatancito',
+                'Cuilco',
+                'Nenton',
+                'San Pedro Necta',
+                'Jacaltenango',
+                'San Pedro Soloma',
+                'San Ildelfonso Ixtahuacán',
+                'Santa Bárbara',
+                'La Libertad',
+                'San Miguel Acatán',
+                'San Rafael La Independencia',
+                'Santa Cruz Barillas',
+                'San Juan Atitán',
+                'Santa Eulalia',
+                'San Mateo Ixtatán',
+                'Colotenango',
+                'San Sebastián Coatan',
+                'San Juan Ixcoy',
+                'Nebaj',
+                'San Antonio Huista',
+                'San Miguel Ixtahuacán',
+                'San Juan Cotzal',
+                'Joyabaj',
+                'Santa Cruz Verapaz',
+                'Tactic',
+                'Tamahú',
+                'Senahú',
+                'Santa María Cahabón',
+                'Lanquín',
+                'Chahal',
+                'Fray Bartolomé de las Casas',
+                'Chisec',
+                'Santa Cruz Verapaz',
+                'Cahabón',
+                'Tamahú',
+                'Tucurú'
+            ]
+        },
+        {
+            nombre: 'Izabal',
+            municipios: [
+                'Puerto Barrios',
+                'Livingston',
+                'El Estor',
+                'Morales',
+                'Los Amates'
+            ]
+        },
+        {
+            nombre: 'Jalapa',
+            municipios: [
+                'Jalapa',
+                'San Pedro Pinula',
+                'San Luis Jilotepeque',
+                'San Manuel Chaparrón',
+                'San Carlos Alzatate',
+                'Monjas',
+                'Mataquescuintla'
+            ]
+        },
+        {
+            nombre: 'Jutiapa',
+            municipios: [
+                'Jutiapa',
+                'El Progreso',
+                'Santa Catarina Mita',
+                'Agua Blanca',
+                'Asunción Mita',
+                'Yupiltepeque',
+                'Atescatempa',
+                'Jerez',
+                'El Adelanto',
+                'Zapotitlán',
+                'Comapa',
+                'Jalpatagua',
+                'Conguaco',
+                'Moyuta',
+                'Pasaco',
+                'San José Acatempa',
+                'Quesada'
+            ]
+        },
+        {
+            nombre: 'Petén',
+            municipios: [
+                'Flores',
+                'San José',
+                'San Benito',
+                'San Andrés',
+                'La Libertad',
+                'San Francisco',
+                'Santa Ana',
+                'Dolores',
+                'San Luis',
+                'Sayaxché',
+                'Melchor de Mencos',
+                'Poptún',
+                'Las Cruces',
+                'La Laguna',
+                'Dolores'
+            ]
+        },
+        {
+            nombre: 'Quetzaltenango',
+            municipios: [
+                'Quetzaltenango',
+                'Salcajá',
+                'Olintepeque',
+                'San Carlos Sija',
+                'Sibilia',
+                'Cabricán',
+                'Cajolá',
+                'San Miguel Sigüilá',
+                'San Juan Ostuncalco',
+                'San Mateo',
+                'Concepción Chiquirichapa',
+                'San Martín Sacatepéquez',
+                'Almolonga',
+                'Cantel',
+                'Huitán',
+                'Zunil',
+                'Colomba',
+                'San Francisco La Unión',
+                'El Palmar',
+                'Coatepeque',
+                'Génova',
+                'Flores Costa Cuca'
+            ]
+        },
+        {
+            nombre: 'Quiché',
+            municipios: [
+                'Santa Cruz del Quiché',
+                'Chiché',
+                'Chinique',
+                'Zacualpa',
+                'Chajul',
+                'Chichicastenango',
+                'Patzité',
+                'San Antonio Ilotenango',
+                'San Pedro Jocopilas',
+                'Cunén',
+                'San Juan Cotzal',
+                'Joyabaj',
+                'Nebaj',
+                'San Pedro Soloma',
+                'San Idelfonso Ixtahuacán',
+                'San Miguel Uspantán',
+                'Santiago Sacatepéquez',
+                'San Juan Ixcoy',
+                'San Mateo Ixtatán',
+                'Santa Cruz La Laguna',
+                'Candelaria',
+                'Tojchic',
+                'Sacapulas',
+                'Sumpango',
+                'Santa Cruz Muluá',
+                'San Bartolomé Jocotenango',
+                'Patzicía',
+                'Santa Catarina Ixtahuacán',
+                'Santa Clara La Laguna',
+                'Concepción',
+                'San Andrés Sajcabajá',
+                'Panajachel',
+                'Santa María Visitación',
+                'Chiché'
+            ]
+        },
+        {
+            nombre: 'Retalhuleu',
+            municipios: [
+                'Retalhuleu',
+                'San Sebastián',
+                'Santa Cruz Muluá',
+                'San Martín Zapotitlán',
+                'San Felipe',
+                'San Andrés Villa Seca',
+                'Champerico',
+                'Nuevo San Carlos',
+                'El Asintal'
+            ]
+        },
+        {
+            nombre: 'Sacatepéquez',
+            municipios: [
+                'La Antigua Guatemala',
+                'Jocotenango',
+                'Magdalena Milpas Altas',
+                'Santa María de Jesús',
+                'Ciudad Vieja',
+                'San Miguel Dueñas',
+                'Santiago Sacatepéquez',
+                'San Bartolomé Milpas Altas',
+                'San Lucas Sacatepéquez',
+                'Santa Lucía Milpas Altas',
+                'Parramos',
+                'San Juan Alotenango',
+                'San Pedro Sacatepéquez',
+                'Santo Domingo Xenacoj'
+            ]
+        },
+        {
+            nombre: 'San Marcos',
+            municipios: [
+                'San Marcos',
+                'San Pedro Sacatepéquez',
+                'San Antonio Sacatepéquez',
+                'Comitancillo',
+                'San Miguel Ixtahuacán',
+                'Concepción Tutuapa',
+                'Tacaná',
+                'Sibinal',
+                'Tajumulco',
+                'Tejutla',
+                'San Rafael Pie de La Cuesta',
+                'Nuevo Progreso',
+                'El Tumbador',
+                'San José El Rodeo',
+                'Malacatán',
+                'Catarina',
+                'Ayutla',
+                'Ocós',
+                'San Pablo'
+            ]
+        },
+        {
+            nombre: 'Santa Rosa',
+            municipios: [
+                'Cuilapa',
+                'Barberena',
+                'Santa Rosa de Lima',
+                'Casillas',
+                'San Rafael las Flores',
+                'Oratorio',
+                'San Juan Tecuaco',
+                'Chiquimulilla',
+                'Taxisco',
+                'Santa María Ixhuatán',
+                'Guazacapán',
+                'Santa Cruz Naranjo',
+                'Pueblo Nuevo Viñas'
+            ]
+        },
+        {
+            nombre: 'Sololá',
+            municipios: [
+                'Sololá',
+                'San José Chacayá',
+                'Santa María Visitación',
+                'Santa Lucía Utatlán',
+                'Nahualá',
+                'Santa Catarina Ixtahuacán',
+                'Santa Clara La Laguna',
+                'Concepción',
+                'San Andrés Semetabaj',
+                'Panajachel',
+                'San Antonio Palopó',
+                'San Lucas Tolimán',
+                'Santa Cruz La Laguna',
+                'San Pablo La Laguna',
+                'San Marcos La Laguna',
+                'Santa Catarina Palopó',
+                'San Juan La Laguna',
+                'San Pedro La Laguna'
+            ]
+        },
+        {
+            nombre: 'Suchitepéquez',
+            municipios: [
+                'Mazatenango',
+                'Cuyotenango',
+                'San Francisco Zapotitlán',
+                'San Bernardino',
+                'San José El Idolo',
+                'Santo Domingo Suchitepéquez',
+                'San Lorenzo',
+                'Samayac',
+                'San Pablo Jocopilas',
+                'San Antonio Suchitepéquez',
+                'San Gabriel',
+                'Chicacao',
+                'Patulul',
+                'Santa Bárbara',
+                'San Juan Bautista',
+                'Santo Tomás La Unión',
+                'Zunilito',
+                'Pueblo Nuevo Suchitepéquez',
+                'Río Bravo'
+            ]
+        },
+        {
+            nombre: 'Totonicapán',
+            municipios: [
+                'Totonicapán',
+                'San Cristóbal Totonicapán',
+                'San Francisco El Alto',
+                'Momostenango',
+                'Santa María Chiquimula',
+                'Santa Lucía La Reforma',
+                'San Andrés Xecul',
+                'San Bartolo',
+                'San Pedro Totonicapán'
+            ]
+        },
+        {
+            nombre: 'Zacapa',
+            municipios: [
+                'Zacapa',
+                'Estanzuela',
+                'Río Hondo',
+                'Gualán',
+                'Teculután',
+                'Usumatlán',
+                'Cabañas',
+                'San Diego'
+            ]
+        }
+    ]
+};
