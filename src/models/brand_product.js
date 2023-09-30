5c8fb2d2d1ecfd1aa93734caee26147f56a1d639
HectorGarcia01 <hg2779948@gmail.com>
2023-09-29 18:54:22 -0600

    Se agregó el fk de estado y se estableció la relacion de uno a uno, además de filtrar las fechas y el fk de estado

--- src/models/brand_product.js
+++ src/models/brand_product.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const db = require('../database/db_connection');
+const Estado = require('../models/state');
 
 /**
  * Creación del modelo Marca del Producto
@@ -12,7 +13,49 @@ const Marca_Producto = db.define('JHSGR_Marca_Producto', {
         type: DataTypes.STRING(50),
         allowNull: false,
         unique: true
+    },
+    ID_Estado_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'JHSGR_Estados',
+            key: 'id'
+        }
     }
 });
+
+/**
+ * Configurando la relación de uno a uno
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Marca_Producto (brand_product.js) -> uno
+ *              Modelo Estado (state.js)  -> uno
+ */
+
+Estado.hasOne(Marca_Producto, {
+    foreignKey: 'ID_Estado_FK'
+});
+
+Marca_Producto.belongsTo(Estado, {
+    foreignKey: 'ID_Estado_FK',
+    as: 'estado'
+});
+
+/**
+ * Método personalizado para filtrar información
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ */
+
+Marca_Producto.prototype.toJSON = function () {
+    const brand_product = { ...this.get() };
+
+    delete brand_product.ID_Estado_FK;
+    delete brand_product.createdAt;
+    delete brand_product.updatedAt;
+
+    return brand_product;
+};
 
 module.exports = Marca_Producto;
