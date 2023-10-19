1dab58ff3089372a9f30f0edc905a8b52edbfbd8
HectorGarcia01 <hg2779948@gmail.com>
2023-10-19 14:10:04 -0600

    Pendiente modificación

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router =  new express.Router();
-const {
+// const {
     
-} = require('../controllers/ALGOALGOALGOALGOALGOALGO.controller');
-const {
+// } = require('../controllers/ALGOALGOALGOALGOALGOALGO.controller');
+// const {
     
-} = require('../schemas/ALGOALGOALGOALGO.schema');
+// } = require('../schemas/ALGOALGOALGOALGO.schema');
 const validateMiddleware = require('../middlewares/validate');
 const authMiddleware = require('../middlewares/auth');
 const roleMiddleware = require('../middlewares/check_role');
