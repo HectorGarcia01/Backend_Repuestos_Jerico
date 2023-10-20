68c56d7ddc80562cf5dc4b4eea1fe450e97fecd0
HectorGarcia01 <hg2779948@gmail.com>
2023-10-20 14:15:18 -0600

    Creación del modelo para el método de pago

---
+++ src/models/shipping_type.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db_connection');
+
+/**
+ * Creación del modelo Metodo_Pago
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ */
+
+const Metodo_Pago = db.define(`JHSGR_Metodo_Pago`, {
+    tipo_pago: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    }
+});
+
+module.exports = Metodo_Pago;
