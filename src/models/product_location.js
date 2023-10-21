7ecec2cc3d46eff903c8d0b24d94f17c53c04e51
HectorGarcia01 <hg2779948@gmail.com>
2023-10-20 21:18:53 -0600

    Se agregó el fk de estado y su respectiva relación de uno a uno

--- src/models/product_location.js
+++ src/models/product_location.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const db = require('../database/db_connection');
+const Estado = require('../models/state');
 
 /**
  * Creación del modelo Ubicacion_Producto
@@ -12,7 +13,33 @@ const Ubicacion_Producto = db.define(`JHSGR_Ubicacion_Producto`, {
         type: DataTypes.STRING(200),
         allowNull: false,
         unique: true
+    },
+    ID_Estado_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'JHSGR_Estados',
+            key: 'id'
+        }
     }
+});
+
+/**
+ * Configurando la relación de uno a uno
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Ubicacion_Producto (product_location.js) -> uno
+ *              Modelo Estado (state.js)  -> uno
+ */
+
+Estado.hasOne(Ubicacion_Producto, {
+    foreignKey: 'ID_Estado_FK'
+});
+
+Ubicacion_Producto.belongsTo(Estado, {
+    foreignKey: 'ID_Estado_FK',
+    as: 'estado'
 });
 
 module.exports = Ubicacion_Producto;
