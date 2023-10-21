46b60bb7256e52ff96726ac51d4c0167b41f7506
HectorGarcia01 <hg2779948@gmail.com>
2023-10-20 18:05:23 -0600

    Creación de la función para agregar o actualizar un producto en el carrito

---
+++ src/controllers/shopping.controller.js
@@ -0,0 +1,94 @@
+const SalesInvoiceModel = require('../models/sales_invoice');
+const SalesDetailModel = require('../models/sales_detail');
+const ProductModel = require('../models/product');
+const StateModel = require('../models/state');
+
+/**
+ * Función para registrar productos al carrito
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Factura_Venta (sales_invoice.js), 
+ *              Modelo Detalle_Venta (sales_detail.js),
+ *              
+ */
+
+const createShoppingCart = async (req, res) => {
+    try {
+        const { user } = req;
+        const { ID_Producto_FK, cantidad_producto } = req.body;
+
+        const product = await ProductModel.findByPk(ID_Producto_FK);
+
+        if (!product) {
+            return res.status(404).send({ error: "Producto no encontrado." });
+        }
+
+        if (product.cantidad_stock < cantidad_producto) {
+            return res.status(400).send({ error: "No hay suficiente stock disponible." });
+        }
+
+        const subtotal_venta = cantidad_producto * product.precio_venta;
+
+        const stateShoppingCart = await StateModel.findOne({
+            where: {
+                nombre_estado: 'Carrito'
+            }
+        });
+
+        if (!stateShoppingCart) {
+            return res.status(404).send({ error: "Estado no encontrado." });
+        }
+
+        let shoppingCart = await SalesInvoiceModel.findOne({
+            where: {
+                ID_Cliente_FK: user.id,
+                ID_Estado_FK: stateShoppingCart.id
+            }
+        });
+        
+        if (!shoppingCart) {
+            shoppingCart = await SalesInvoiceModel.create({
+                nit_cliente: user.nit,
+                total_factura: 0,
+                ID_Cliente_FK: user.id,
+                ID_Estado_FK: stateShoppingCart.id
+            });
+        }
+
+        let shoppingCartDetail = await SalesDetailModel.findOne({
+            where: {
+                ID_Factura_Venta_FK: shoppingCart.id,
+                ID_Producto_FK
+            }
+        });
+
+        if (shoppingCartDetail) {
+            shoppingCartDetail.cantidad_producto += cantidad_producto;
+            shoppingCartDetail.subtotal_venta += subtotal_venta;
+            await shoppingCartDetail.save();
+        } else{
+            shoppingCartDetail = await SalesDetailModel.create({
+                cantidad_producto,
+                precio_unitario: product.precio_venta,
+                subtotal_venta,
+                ID_Producto_FK,
+                ID_Factura_Venta_FK: shoppingCart.id
+            });
+        }
+
+        shoppingCart.total_factura +=  subtotal_venta;
+        await shoppingCart.save();
+
+        product.cantidad_stock -= cantidad_producto;
+        await product.save();
+
+        res.status(201).send({ msg: "Producto agregado al carrito con éxito." });
+    } catch (error) {
+        res.status(500).send({ error: "Error interno del servidor." });
+    }
+};
+
+module.exports = { 
+    createShoppingCart
+}
