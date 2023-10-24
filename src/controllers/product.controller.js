0e505f0717124f3fc1f9fb29df672c26be3ecc8a
HectorGarcia01 <hg2779948@gmail.com>
2023-10-24 02:38:50 -0600

    Optimización de la función para ver todos los productos, se mandó a llamar la función reutilizable para generar la consulta where

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,6 +4,7 @@ const StateModel = require('../models/state');
 const CategoryModel = require('../models/category');
 const BrandProductModel = require('../models/brand_product');
 const ProductLocationModel = require('../models/product_location');
+const buildWhereClause = require('../utils/build_where_clause');
 
 /**
  * Función para registrar un nuevo producto
@@ -96,116 +97,13 @@ const createProduct = async (req, res) => {
  * Autor: Hector Armando García González
  * Referencias:
  *              Modelo Producto (product.js), 
+ *              Función buildWhereClause (build_where_clause.js)
  */
 
 const readProducts = async (req, res) => {
     try {
-        const { query } = req;
-        const where = {};
-
-        if (query.nombre) {
-            where.nombre_producto = {
-                [Sequelize.Op.like]: `%${query.nombre}%`
-            };
-        }
-
-        if (query.codigo) {
-            where.codigo_producto = {
-                [Sequelize.Op.like]: `%${query.codigo}%`
-            };
-        }
-
-        if (query.precio_compra) {
-            where.precio_compra = {
-                [Sequelize.Op.gte]: query.precio_compra
-            };
-        }
-
-        if (query.precio_venta) {
-            where.precio_venta = {
-                [Sequelize.Op.gte]: query.precio_venta
-            };
-        }
-
-        if (query.descripcion) {
-            where.descripcion_producto = {
-                [Sequelize.Op.like]: `%${query.descripcion}%`
-            };
-        }
-
-        if (query.medida) {
-            where.medida_producto = {
-                [Sequelize.Op.like]: `%${query.medida}%`
-            };
-        }
-
-        if (query.color) {
-            where.color_producto = {
-                [Sequelize.Op.like]: `%${query.color}%`
-            };
-        }
-
-        if (query.QR_producto) {
-            where.QR_producto = {
-                [Sequelize.Op.like]: `%${query.QR_producto}%`
-            };
-        }
-
-        if (query.estado) {
-            const stateCustomer = await StateModel.findOne({
-                where: {
-                    nombre_estado: query.estado
-                }
-            });
-
-            if (!stateCustomer) {
-                return res.status(404).send({ error: "Estado no encontrado." });
-            }
-
-            where.ID_Estado_FK = stateCustomer.id
-        }
-
-        if (query.categoria) {
-            const category = await CategoryModel.findOne({
-                where: {
-                    nombre_categoria: query.categoria
-                }
-            });
-
-            if (!category) {
-                return res.status(404).send({ error: "Categoría no encontrada." });
-            }
-
-            where.ID_Categoria_FK = category.id;
-        }
-
-        if (query.marca) {
-            const brandProduct = await BrandProductModel.findOne({
-                where: {
-                    nombre_marca: query.marca
-                }
-            });
-
-            if (!brandProduct) {
-                return res.status(404).send({ error: "Marca no encontrada." });
-            }
-
-            where.ID_Marca_FK = brandProduct.id;
-        }
-
-        if (query.ubicacion) {
-            const productLocation = await ProductLocationModel.findOne({
-                where: {
-                    nombre_estanteria: query.ubicacion
-                }
-            });
-
-            if (!productLocation) {
-                return res.status(404).send({ error: "Ubicación no encontrada." });
-            }
-
-            where.ID_Ubicacion_FK = productLocation.id;
-        }
+        const where = await buildWhereClause(req.query);
+        console.log(where);
 
         const products = await ProductModel.findAll({ 
             where,
