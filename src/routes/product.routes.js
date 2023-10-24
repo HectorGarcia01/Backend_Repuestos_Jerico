c4bac973d2de51bb6b2eb5478ce0e6c27c3579c9
HectorGarcia01 <hg2779948@gmail.com>
2023-10-23 21:45:29 -0600

    Implementación del esquema de validación para los endpoints de actualización de productos

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -8,24 +8,49 @@ const {
     deleteProductId
 } = require('../controllers/product.controller');
 const {
-    productSchema
+    productSchema,
+    updateProductSchema
 } = require('../schemas/product.schema');
 const validateMiddleware = require('../middlewares/validate');
 const authMiddleware = require('../middlewares/auth');
 const roleMiddleware = require('../middlewares/check_role');
 
 //Rutas (endpoints) para el SuperAdmin
-router.post('/superAdmin/crear/producto', authMiddleware, roleMiddleware('SuperAdmin'), validateMiddleware(productSchema), createProduct);
+router.post(
+    '/superAdmin/crear/producto', 
+    authMiddleware, 
+    roleMiddleware('SuperAdmin'), 
+    validateMiddleware(productSchema), 
+    createProduct
+);
 router.get('/superAdmin/ver/productos', authMiddleware, roleMiddleware('SuperAdmin'), readProducts);
 router.get('/superAdmin/ver/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), readProductId);
-router.patch('/superAdmin/actualizar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), updateProductId);
+router.patch(
+    '/superAdmin/actualizar/producto/:id', 
+    authMiddleware, 
+    roleMiddleware('SuperAdmin'), 
+    validateMiddleware(updateProductSchema), 
+    updateProductId
+);
 router.delete('/superAdmin/eliminar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteProductId);
 
 //Rutas (endpoints) para el Admin
-router.post('/admin/crear/producto', authMiddleware, roleMiddleware('Admin'), validateMiddleware(productSchema), createProduct);
+router.post(
+    '/admin/crear/producto',
+    authMiddleware, 
+    roleMiddleware('Admin'), 
+    validateMiddleware(productSchema), 
+    createProduct
+);
 router.get('/admin/ver/productos', authMiddleware, roleMiddleware('Admin'), readProducts);
 router.get('/admin/ver/producto/:id', authMiddleware, roleMiddleware('Admin'), readProductId);
-router.patch('/admin/actualizar/producto/:id', authMiddleware, roleMiddleware('Admin'), updateProductId);
+router.patch(
+    '/admin/actualizar/producto/:id', 
+    authMiddleware, 
+    roleMiddleware('Admin'), 
+    validateMiddleware(updateProductSchema),
+    updateProductId
+);
 
 //Rutas (endpoints) para el home
 router.get('/usuario/ver/productos', readProducts);
