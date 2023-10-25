ed45b4c7764c459c8837af67cdf0a2242945dd30
HectorGarcia01 <hg2779948@gmail.com>
2023-10-24 20:17:22 -0600

    Configuración de los endpoints para ver un inventario por id

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router =  new express.Router();
 const {
-    readInventories
+    readInventories,
+    readInventoryId
 } = require('../controllers/inventory.controller');
 const authMiddleware = require('../middlewares/auth');
 const roleMiddleware = require('../middlewares/check_role');
 
 //Configuración de rutas (endpoints) para el SuperAdmin
 router.get('/superAdmin/ver/inventarios', authMiddleware, roleMiddleware('SuperAdmin'), readInventories);
+router.get('/superAdmin/ver/inventario/:id', authMiddleware, roleMiddleware('SuperAdmin'), readInventoryId);
 
 //Configuración de rutas (endpoints) para el Admin
 router.get('/admin/ver/inventarios', authMiddleware, roleMiddleware('Admin'), readInventories);
+router.get('/admin/ver/inventario/:id', authMiddleware, roleMiddleware('Admin'), readInventoryId);
 
 module.exports = router;
