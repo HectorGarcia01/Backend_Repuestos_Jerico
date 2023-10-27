7db111e68076552a1b7384b2ddde69b7dbfc6c01
HectorGarcia01 <hg2779948@gmail.com>
2023-10-27 05:00:17 -0600

    Creación de la función para enviar un correo de bienvenida después de que la persona haya activado su cuenta

---
+++ src/email/welcome_email.js
@@ -0,0 +1,23 @@
+const transporter = require('./email');
+const fs = require('fs');
+const path = require('path');
+
+const welcomeEmail = async (destination) => {
+    try {
+        const pathHTML = path.join(__dirname, './public/welcome.html');
+        const html = fs.readFileSync(pathHTML, 'utf8');
+
+        const mailOptions = {
+            from: "Venta de repuestos Jericó",
+            to: destination,
+            subject: "Tu cuenta ha sido verificada.",
+            html
+        };
+        
+        const info = await transporter.sendMail(mailOptions);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+module.exports = welcomeEmail;
